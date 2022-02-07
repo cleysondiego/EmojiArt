40d22ebc8d0e1d8411526247301aa2ba44b06a34
Cleyson Silva <cdsilva1@topaz.com.br>
2022-02-07 20:58:08 -0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Cleyson Silva on 07/02/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
